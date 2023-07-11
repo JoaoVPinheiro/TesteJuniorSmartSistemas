4e3d1b0ce4ddf70ac965bf15677e5c9333ef8090
JoaoVPinheiro <54337739+JoaoVPinheiro@users.noreply.github.com>
2023-07-11 08:28:21 -0300

    Add files via upload

---
+++ env.exemplo.php
@@ -0,0 +1,11 @@
+<?php
+
+putenv('DISPLAY_ERRORS_DETAILS=' . true);
+
+
+putenv('TESTESMART_MYSQL_HOST=localhost');
+putenv('TESTESMART_MYSQL_DBNAME=testesmart');
+putenv('TESTESMART_MYSQL_USER=root');
+putenv('TESTESMART_MYSQL_PASSWORD=');
+putenv('TESTESMART_MYSQL_PORT=3306');
+
